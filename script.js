19ce754bd95df4274ee8e3bf8f124e44d1c57694
Adrián Furtado <adrianfurtado.dev@gmail.com>
2023-09-11 23:22:46 -0300

    El código ha sido mejorado para una mayor legibilidad.

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-/*
- * Nuestra solución cuenta con una cantidad de funciones
- * superior a las dadas en primera instancia. Esta decisión 
- * viene a raíz de una modularización y legibilidad mayor del código.
- */
-
-
 const URL = 'https://fakestoreapi.com/products'
 const container = document.querySelector('#products')
 
@@ -16,49 +9,38 @@ async function fetchData(url) {
     const response = await fetch(url);
     const data = await response.json();
     products = data;
-
     showProducts();
 }
-
-// La función 'addProduct' agrega un producto en el HTML.
-const addProduct = product => {
-    container.innerHTML += `
-    <div class="list-group-item">
-        <div class="fw-bold">${cutString(product.title)}</div>
-        <div class="d-flex gap-2">
-            <span>${new Date().toLocaleString()}</span>
-            ${stars(Math.floor(product.rating.rate)).outerHTML}
-        </div>
-    </div>
-    `;
-};
 
 // Esta función recorre el array 'products' y lo muestra uno por uno.
 function showProducts() {
     products.forEach(product => {
-        addProduct(product);
+        container.innerHTML += `
+        <div class="list-group-item">
+            <div class="fw-bold">${cutString(product.title)}</div>
+            <div class="d-flex gap-2">
+                <span>${new Date().toLocaleString()}</span>
+                <div>
+                    ${stars(Math.floor(product.rating.rate), 5)}
+                </div>
+            </div>
+        </div>
+        `;
     });
 }
 
-// Función que retorna el código HTML de una estrella
-const addStar = (iteration, cantidad) => {
-    return `<i class="fa fa-star" style="color: ${iteration < cantidad ? '#ffa200' : '#252525'};"></i>`;
-};
-
-// Esta función crea un elemento 'div' que termina retornando con todas las estrellas.
-function stars(cantidad) {
-    const products = document.createElement('div');
-    for (i = 0; i < 5; i++) {
-        products.innerHTML += addStar(i, cantidad);
+// Crea una variable que se inicializa con una cadena vacía y se le agrega cada una de las estrellas. Al finalizar, retorna esta variable.
+function stars(cantidad, maxStars) {
+    let starsHTML = '';
+    for (i = 0; i < maxStars; i++) {
+        starsHTML += `<i class="fa fa-star" style="color: ${i < cantidad ? '#ffa200' : '#252525'};"></i>`
     }
-    return products;
+    return starsHTML;
 }
 
 
-/* 
- * Si el título supera los 20 carácteres, este se "corta",
- * mostrando signos de puntación al superar este límite.
- */
+// Si el String, que recibe como parámetro, supera los 20 caracteres de largo,
+// se elimina el sobrante y se remplaza por la cadena de texto '...'.
 function cutString(string) {
     return string.length > 20 ? string.slice(0, 20) + '...' : string;
 }
